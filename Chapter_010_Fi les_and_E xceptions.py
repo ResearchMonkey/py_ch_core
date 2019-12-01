d4e12a7e85e87203c8a9261cdc66edde672408fa
researchmonkey <research_monkey@live.com>
2019-12-01 13:58:27 -0600

    write to file 001

--- Chapter_010_Fi les_and_E xceptions.py
+++ Chapter_010_Fi les_and_E xceptions.py
@@ -9,28 +9,37 @@ my_dir = os.getcwd()
 
 # filename = 'pi_digits.txt'
 # filename = '{}/python_crash_course_files/chapter_10/pi_digits.txt'.format(my_dir)
-filename = '{}/python_crash_course_files/chapter_10/pi_million_digits.txt'.format(my_dir)
+# filename = '{}/python_crash_course_files/chapter_10/pi_million_digits.txt'.format(my_dir)
 
 # with open(filename) as file_object:
 #     for line in file_object:
 #         print(line.rstrip())
 
-with open(filename) as file_object:
-    lines = file_object.readlines()
-
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
+#
+# # for line in lines:
+# #     print(line.rstrip())
+#
+# pi_string = ''
 # for line in lines:
-#     print(line.rstrip())
+#     pi_string += line.strip()
+#
+# birthday = input("Enter your birthday, in the form of mmddyy")
+# if birthday in pi_string:
+#     print("Your birthday is in the first million digits of pi")
+# else:
+#     print("your not in pi")
+#
+#
+# print(pi_string)
+# print(len(pi_string))
 
-pi_string = ''
-for line in lines:
-    pi_string += line.strip()
+filename = '{}/python_crash_course_files/chapter_10/programming.txt'.format(my_dir)
 
-birthday = input("Enter your birthday, in the form of mmddyy")
-if birthday in pi_string:
-    print("Your birthday is in the first million digits of pi")
-else:
-    print("your not in pi")
+with open(filename, 'w') as file_object:
+    file_object.write('I love Programming, well love is a strong word. \n')
+    file_object.write('I love creating new games.')
 
-
-print(pi_string)
-print(len(pi_string))
+with open(filename, 'a') as file_object:
+    file_object.write("\nTHis is a new line")
