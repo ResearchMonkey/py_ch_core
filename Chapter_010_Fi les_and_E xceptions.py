8f534f82537a68a14c4153d0af71cc465508c6b1
research_monkey <research_monkey@live.com>
2019-12-01 21:04:02 -0600

    page 210

--- Chapter_010_Fi les_and_E xceptions.py
+++ Chapter_010_Fi les_and_E xceptions.py
@@ -92,3 +92,19 @@ my_dir = os.getcwd()
 #     count_words(files)
 
 """"Storing Data page 208"""
+
+import json
+
+numbers = [2, 3, 5, 7, 11, 13]
+
+filename = 'numbers.json'
+
+# with open(filename, 'w') as f_obj:
+#     json.dump(numbers, f_obj)
+
+# with open(filename) as f_obj:
+#     numbers = json.load(f_obj)
+#
+# print(numbers)
+
+"""Saving and Reading User-Generated Data page 210"""
